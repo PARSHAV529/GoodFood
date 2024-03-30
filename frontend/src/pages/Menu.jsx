cb68369300cf6d06e80fa17a7cfc02270904f4cc
Parshav <parshav143@gmail.com>
2024-03-30 18:24:59 +0530

    deploy

--- frontend/src/pages/Menu.jsx
+++ frontend/src/pages/Menu.jsx
@@ -1,74 +1,104 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchProducts, selectAllProducts } from '../store/menu/productsSlice';
-import { addToCart } from "../store/cart/cartSlice";
-import { FallbackMenu } from "../components/FallbackMenu";
+import {fetchProducts,selectAllProducts} from '../store/menu/productsSlice'
+import ProductDetailCard from '../components/ProductDetailCard'
 import { Tabs } from "../components/Tabs";
+import { addToCart } from "../store/cart/cartSlice";
 import { ProductPreviewCard } from "../components/ProductPreviewCard";
 import toast from "react-hot-toast";
 import { user } from "../store/userInfo/userSlice";
+import { FallbackMenu } from "../components/FallbackMenu";
 
-const Menu = () => {
+
+
+
+const Menu = ()=>{
+
     const dispatch = useDispatch();
-    const userinfo = useSelector(user);
-    const email = userinfo.providerid;
+    const userinfo = useSelector(user)
+    const email = userinfo.providerid
+    console.log(userinfo.providerid)
 
+    
     useEffect(() => {
-        if (email) {
-            dispatch(fetchProducts(email));
-        }
-    }, [dispatch, email]);
+        dispatch(fetchProducts(email))
+        console.log(userinfo.providerid)        
+    }, [userinfo,dispatch])
+
 
     const products = useSelector(selectAllProducts);
-    const [searchItem, setSearchItem] = useState('');
+    console.log(products)
+    const [filterdProducts,setFilterdProducts]= useState(products);
+    
+    const [searchItem, setSearchItem] = useState('')
+    // console.log(filterdProducts)
+    // console.log(JSON.stringify(products))
     const [activeTab, setActiveTab] = useState('');
     const [activeTabIndex, setActiveTabIndex] = useState(0);
 
     const onAddProduct = (product) => {
-        dispatch(addToCart(product));
-        toast.success("Product added to cart!");
-    };
-
+        
+        // toast.success("Successfully toasted!")
+        dispatch(addToCart(product))
+    }
     const onTabSwitch = (newActiveTab) => {
         setActiveTab(newActiveTab);
-        const index = products.findIndex(product => product.name.name === newActiveTab);
-        setActiveTabIndex(index >= 0 ? index : 0);
-    };
+        let categories = products.products.map((product) => product.name.name);
+        let index = categories.findIndex(category => newActiveTab === category);
+        console.log(index);
+        if (index > -1) {
+            setActiveTabIndex(index);
+        } else {
+            setActiveTabIndex(0);
+        }
+    }
 
-    const handleInputChange = (e) => {
-        const searchTerm = e.target.value.toLowerCase();
-        setSearchItem(searchTerm);
-        const filteredProducts = products[activeTabIndex]?.products.filter(product =>
-            product.name.toLowerCase().includes(searchTerm)
-        );
-        setFilterdProducts(filteredProducts || []);
-    };
-
-    return (
-        email ?
-            <div className="bg-[#fff] mt-10 h-screen">
-                {products && products.status !== 'fulfilled' ?
-                    <div className="loader" /> :
-                    <div className="menu-wrapper">
-                        {products &&
-                            <Tabs
-                                list={products.map((product) => product.name.name)}
-                                activeTab={activeTab}
-                                onTabSwitch={onTabSwitch}
-                                handleInputChange={handleInputChange}
-                                searchItem={searchItem}
-                            />
-                        }
-                        <div className="flex flex-row mx-4 justify-center flex-wrap">
-                            {products[activeTabIndex]?.products.map((product, index) => (
-                                <ProductPreviewCard key={index} product={product} onAddProduct={onAddProduct} />
-                            ))}
-                        </div>
-                    </div>
+    const handleInputChange = (e) => { 
+        const searchTerm = e.target.value;
+        setSearchItem(searchTerm)
+    
+        for (const product of product.products) {
+            for (const subProduct of product.products) {
+                // Check if product name matches
+                if (subProduct.name.toLowerCase() === searchTerm.toLowerCase()) {
+                    setFilterdProducts(subProduct)
                 }
-            </div> :
-            <FallbackMenu />
-    );
-};
+            }
+        }
+      }
 
-export default Menu;
+    return(
+        email ?
+        
+        <div className="bg-[#fff] mt-10 h-screen">
+           {
+           products && products.status !== 'fulfilled' ?
+            <div className="loader " /> :
+            <div className="menu-wrapper ">
+                {
+                    products.products &&
+                    <Tabs
+                        list={products.products.map((product) => product.name.name)}
+                        activeTab={activeTab}
+                        onTabSwitch={onTabSwitch}
+                        handleInputChange={handleInputChange}
+                        searchItem={searchItem}
+                        
+                        />
+                }
+                <div className="flex flex-row mx-4 justify-center  flex-wrap">
+                {
+                    products.products && products.products[activeTabIndex].products.map((product, index) => {
+                        return (
+                           <ProductPreviewCard key={index} product={product} onAddProduct={onAddProduct}/>
+                        )
+                    })
+                }
+                </div>
+            </div>
+           }
+        </div>:<FallbackMenu/>
+    )
+}
+
+export default Menu
